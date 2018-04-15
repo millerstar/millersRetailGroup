73eee57717a592faa2854e3300d6b1b211d42c39
idan <idan006@gmail.com>
2018-04-15 15:27:11 +0300

    Menu - cont

--- src/main/java/sql_practice/Menu.java
+++ src/main/java/sql_practice/Menu.java
@@ -3,15 +3,15 @@ package sql_practice;
 import java.util.Scanner;
 
 public class Menu {
-    Scanner userSelection;
+    private static Scanner userSelection;
 
-    public void start() {
+    public static void start() {
         boolean exit=false;
-
+        System.out.println("Welcome to the Miller's Shops DB Manager, please enter your choice, 0 to exit");
+        printMenu();
         while (exit == false) {
-            System.out.println("Welcome to the Miller's Shops DB Manager, please enter your choice, 0 to exit");
             switch (getUserSelection()) {
-                case 0:
+                case 0: exit=true;
                 case 1:
                 case 2:
                 case 3:
@@ -26,21 +26,25 @@ public class Menu {
     }
 
 
-    private void printMenu() {
+    private static void printMenu() {
         System.out.println();
-        System.out.println("/t0. EXIT Program");
-        System.out.println("/t1. Create a new Chain");
-        System.out.println("/t2. Add a store to a Chain");
-        System.out.println("/t3. Add Employee to Chain / to Group Management");
-        System.out.println("/t4. Present all shops that are in a certain Shopping Mall");
-        System.out.println("/t5. Present all shops that are in a certain Shopping Mall Group");
-        System.out.println("/t6. Present all Employees of a certain Chain");
-        System.out.println("/t7. Present all details of a Shop");
+        System.out.println("\t0. EXIT manager");
+        System.out.println("\t1. Create a new Chain");
+        System.out.println("\t2. Add a store to a Chain");
+        System.out.println("\t3. Add Employee to Chain / to Group Management");
+        System.out.println("\t4. Present all shops that are in a certain Shopping Mall");
+        System.out.println("\t5. Present all shops that are in a certain Shopping Mall Group");
+        System.out.println("\t6. Present all Employees of a certain Chain");
+        System.out.println("\t7. Present all details of a Shop");
     }
 
-    private int getUserSelection() {
+    private static int getUserSelection() {
         userSelection = new Scanner(System.in);
         return userSelection.nextInt();
+    }
+
+    public static void main (String[] args){
+        start();
     }
 
 
